9d1aad6146e530176f34137da0a98b12cbf75a16
Antoine Haddaoui <antoine.haddaoui@gmail.com>
2016-08-22 15:32:45 +0200

    Side Navigation
    Side bar for navigation added

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function swagAnim(x) {
+    x.classList.toggle("animate");
+}
+function opencloseNav() {
+    $("#main").toggleClass("reduced");
+    $(".buttonmenu").toggleClass("reduced");
+    $("#sidenav").toggleClass("expanded");
+}
